8acbbad0e0decf3bd72cd0ff9af2ae095420c7ef
Taha Dostifam <mr.tahadostifam@gmail.com>
2022-08-13 14:32:43 +0430

    Init: user_service.go

--- app/usecase/user/user_service.go
+++ app/usecase/user/user_service.go
@@ -1,1 +1,21 @@
 package user
+
+import "github.com/tahadostifam/go-clean-architecture/app/entity"
+
+type UserService struct {
+	userRepo entity.UserRepository
+}
+
+func NewUserService(userRepo entity.UserRepository) *UserService {
+	return &UserService{userRepo: userRepo}
+}
+
+func (service *UserService) GetUser(id string) (*entity.User, error) {
+	return service.userRepo.GetUser(id)
+}
+func (service *UserService) CreateUser(username, email, password string) error {
+	return service.userRepo.CreateUser(username, email, password)
+}
+func (service *UserService) DeleteUser(id string) error {
+	return service.userRepo.DeleteUser(id)
+}
