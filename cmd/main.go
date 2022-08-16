161a8c8444c412d6cc1a6fd7f3e70659c7804f02
Tirouzh <mohammad.kolagar@gmail.com>
2022-08-16 05:43:38 +0430

    Implement auth router

--- cmd/main.go
+++ cmd/main.go
@@ -5,19 +5,28 @@ import (
 	"log"
 
 	"lms/config"
+	"lms/internal/adapters/controller"
+	"lms/internal/adapters/repository/memory"
+	"lms/internal/app/auth"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server := gin.Default()
-
 	cfg, cfgErr := config.Parse()
 	if cfgErr != nil {
 		log.Fatal("failed to load config file", cfgErr)
 	}
 
+	userRepo := memory.NewUserRepo()
+	authService := auth.NewService(userRepo)
+	authCtrl := controller.NewAuthController(authService)
+
+	r := gin.Default()
+	auth := r.Group("/api/auth")
+	auth.POST("/signup", authCtrl.SignUp)
+
 	addr := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
 	log.Printf("server listening on %s\n", addr)
-	server.Run(addr)
+	r.Run(addr)
 }
