5df17ecc9e0afaa7b1a3185e26facd96038ee195
Tirouzh <mohammad.kolagar@gmail.com>
2022-08-18 21:22:27 +0430

    Write user helper test

---
+++ internal/domain/user/user_helper_test.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserHelper_NewUser(t *testing.T) {
+
+	usr := NewUser(USER_TYPE_STUDENT)
+
+	assertNotEmptyUser(t, usr)
+	assert.True(t, usr.IsStudent())
+	assert.False(t, usr.IsVerified())
+	assert.False(t, usr.IsBanned())
+}
+
+func TestUserHelper_NewVerifiedUser(t *testing.T) {
+
+	usr := NewVerifiedUser(USER_TYPE_TEACHER)
+
+	assertNotEmptyUser(t, usr)
+	assert.True(t, usr.IsTeacher())
+	assert.True(t, usr.IsVerified())
+	assert.False(t, usr.IsBanned())
+}
+
+func TestUserHelper_NewBannedUser(t *testing.T) {
+
+	usr := NewBannedUser(USER_TYPE_ADMIN)
+
+	assertNotEmptyUser(t, usr)
+	assert.True(t, usr.IsAdmin())
+	assert.True(t, usr.IsVerified())
+	assert.True(t, usr.IsBanned())
+}
+
+func assertNotEmptyUser(t *testing.T, usr User) {
+	assert.NotEmpty(t, usr.ID.String())
+	assert.NotEmpty(t, usr.Username)
+	assert.NotEmpty(t, usr.Email)
+	assert.NotEmpty(t, usr.passwordHash)
+	assert.NotEmpty(t, usr.CreatedAt)
+	assert.NotEmpty(t, usr.UpdatedAt)
+}
